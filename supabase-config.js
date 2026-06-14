24e7226682f8f5b6c710ef1617c0277b06891eee
hectorvss <75950123+hectorvss@users.noreply.github.com>
2026-06-14 18:49:32 +0200

    Configure Supabase public client

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.ACONTRANOVELA_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://kqzsiydffnuixwgyaups.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxenNpeWRmZm51aXh3Z3lhdXBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0MzUxMzQsImV4cCI6MjA5NzAxMTEzNH0.3VZwuHHhZBFjvqmDJgVzp5ZTPOzw5c0Y6FntOai_0Bc",
   managerEmail: "",
 };
